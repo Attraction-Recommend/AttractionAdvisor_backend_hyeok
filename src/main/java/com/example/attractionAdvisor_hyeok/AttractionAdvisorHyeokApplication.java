8b7a52458b58ec9bfcbb21f391d5648f9a011eb8
pulbak <hl2dop@naver.com>
2024-09-23 14:03:02 +0900

    first commit

---
+++ src/main/java/com/example/attractionAdvisor_hyeok/AttractionAdvisorHyeokApplication.java
@@ -0,0 +1,15 @@
+package com.example.attractionAdvisor_hyeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttractionAdvisorHyeokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttractionAdvisorHyeokApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/example/attractionAdvisor_hyeok/AttractionAdvisorHyeokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attractionAdvisor_hyeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttractionAdvisorHyeokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
